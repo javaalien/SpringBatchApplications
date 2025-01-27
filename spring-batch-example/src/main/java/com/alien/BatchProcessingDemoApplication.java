935ebce8ae2127f38a67710fba1e19800bbb546a
Chetan Malabade <malabade.chetan@gmail.com>
2025-01-28 00:05:20 +0530

    spring batch example

--- spring-batch-example/src/main/java/com/alien/BatchProcessingDemoApplication.java
+++ spring-batch-example/src/main/java/com/alien/BatchProcessingDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchInbuildWriterApplication {
+public class BatchProcessingDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchInbuildWriterApplication.class, args);
+		SpringApplication.run(BatchProcessingDemoApplication.class, args);
 	}
 
 }
